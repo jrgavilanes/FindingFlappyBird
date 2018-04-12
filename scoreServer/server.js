78c69b9eb3970eb5c5a18a445c3abaac7a951149
jrgavilanes <jrgavilanes@gmail.com>
2018-04-12 20:27:27 +0200

    empiezo a guardar datos en sqlite

--- scoreServer/server.js
+++ scoreServer/server.js
@@ -2,7 +2,18 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var expressSanitizer = require('express-sanitizer');
 
-var crypto = require("crypto");
+var crypto = require("crypto"); //Esto igual sobra, ahora está en un modulo nativo.
+
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./database.db', (err) => {
+    if (err) throw err;
+
+});
+
+db.run("CREATE TABLE IF NOT EXISTS scores (user string, score int);");
+
+
+
 
 
 var app = express();
@@ -17,14 +28,33 @@ const SALT = "oJETE78@flipasMIL1009";
 
 //Routes
 
-app.get("/score", function(req, res){
+app.get("/score", function (req, res) {
 
     // res.send("JRG ..... 10#PER ..... 20#JUL ..... 5#RAB ..... 11");
-    res.send("JRG 1000#PER 5");
+    //res.send("JRG 1000#PER 5");
+
+    let sql = ` SELECT rowid, user, score  
+                FROM scores 
+                ORDER BY score DESC, rowid ASC
+                LIMIT 5;`;
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+
+        let salida = "";
+        rows.forEach((row) => {
+            salida += `${row.user} ${row.score}#` 
+            //console.log(row.name);
+        });
+        salida = salida.substr(0, salida.length-1);
+        res.send(salida);
+    });
 
 })
 
-app.get("/score-min", function(req, res){
+app.get("/score-min", function (req, res) {
 
     console.log("devuelvo");
     res.send("1");
@@ -32,24 +62,38 @@ app.get("/score-min", function(req, res){
 })
 
 
-app.post("/score", function(req, res){
+app.post("/score", function (req, res) {
 
-//console.log(req);
-    
+    //console.log(req);
+
     console.log(req.body);
 
-    let {USER, SCORE, hash} = req.body;
+    let { USER, SCORE, hash } = req.body;
 
-    let palabra = crypto.createHash("md5").update(SALT+USER+SCORE).digest("hex");
-    
-    if (palabra === hash){
+
+
+    let palabra = crypto.createHash("md5").update(SALT + USER + SCORE).digest("hex");
+
+    if (palabra === hash) {
         console.log("SÍ te creo");
+
+        USER = req.sanitize(USER);
+        USER = USER.trim().toUpperCase();
+        SCORE = req.sanitize(SCORE);
+
+        // let SQL = `INSERT INTO scores(user,score) VALUES('${USER}',${SCORE});`;
+        // console.log(SQL);
+
+        db.run(`INSERT INTO scores(user,score) VALUES(?,?);`, [USER, SCORE], (err) => {
+            if (err) console.log(err);
+        });
+
     } else {
         console.log("NO te creo");
     }
-    
 
-    
+
+
     // descripcion = req.sanitize(descripcion);
 
     // let nota = {};
@@ -63,14 +107,14 @@ app.post("/score", function(req, res){
 });
 
 
-app.get("*", function(req, res){
+app.get("*", function (req, res) {
 
     res.send("404. Page not found");
 
 });
 
 //Server starts listening.
-app.listen(PORT, IP, function(){
+app.listen(PORT, IP, function () {
 
     console.log("Server listening ... Port: " + PORT + ", IP: " + IP);
 
